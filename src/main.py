08202b95a55acf56b234da618a512f5059075b12
max.kerker <maximilian.lkp@gmail.com>
2026-01-31 23:43:15 +0100

    Initial Commit

---
+++ src/main.py
@@ -0,0 +1,71 @@
+from src import *
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+import sys
+
+config_file = "/home/maxlkp/.telegram_config.yaml"
+with open(config_file, 'r') as file:
+    config = yaml.safe_load(file)
+
+api_token = config["telegram"]["token"]
+chat_id = config["telegram"]["group_id"]
+
+help_message = "Benutzung: \n /gericht + <mensa>: Heutiges Menü der gewählten Mensa \n /gericht + <mens> + <tag> + <datum>: Menü der Mensa an diesem Datum \n Bsp: /gericht vita Dienstag 03.02.2026"
+
+async def respond_gerichte_vita(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    if update.effective_chat.id == chat_id:
+        try:
+            if len(context.args) == 3:
+                mensa = context.args[0]
+                day = context.args[1]
+                date = context.args[2]
+                gerichte = get_gerichte(mensa, day = day, date = date)
+            elif len(context.args) == 1:
+                mensa = context.args[0]
+                gerichte = get_gerichte(mensa)
+            else: pass
+            output = ""
+            for key, gericht in gerichte.items():
+                output = output + f"{key}: {gerichte[key]} \n"
+            await update.message.reply_text(output.encode('latin-1').decode('utf-8'))
+        except Exception as e:
+            print(e)
+            await update.message.reply_text("An error occoured.")
+    else:
+        await context.application.shutdown()
+        sys.exit(0)
+
+async def stop(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    if update.effective_chat.id == chat_id:
+        await update.message.reply_text("Shutting down application.")
+        await context.application.stop()
+        await context.application.shutdown()
+        stop_all()
+        sys.exit(0)
+    else:
+        await context.application.stop()
+        await context.application.shutdown()
+        sys.exit(0)
+
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    if update.effective_chat.id == chat_id:
+        await update.message.reply_text(help_message)
+    else:
+        await context.application.stop()
+        await context.application.shutdown()
+        sys.exit(0)
+
+async def stop_all():
+    sys.exit(0)
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(api_token).build()
+    help_handler = CommandHandler("help", help)
+    gericht_handler = CommandHandler('gericht', respond_gerichte_vita)
+    stop_handler = CommandHandler('stop', stop)
+    application.add_handler(help_handler)
+    application.add_handler(gericht_handler)
+    application.add_handler(stop_handler)
+    application.run_polling()
+
+sys.exit(0)
---
+++ src/src.py
@@ -0,0 +1,59 @@
+import requests
+import re
+from datetime import datetime
+import yaml
+
+days = {"Monday": "Montag", "Tuesday": "Dienstag", "Wednesday": "Mittwoch", "Thursday": "Donnerstag", "Friday": "Freitag", "Saturday": "Samdtag", "Sunday": "Sonntag"} 
+
+def get_config():
+    with open("/home/maxlkp/mensa_bot/src/config.yaml", "r") as file:
+        config = yaml.safe_load(file)
+    return config
+
+def get_day_date():
+    date = datetime.now().strftime("%d.%m.%Y")
+    day = days[f"{datetime.now().strftime("%A")}"]
+    return date, day
+
+def get_gerichte(mensa, day = None, date = None):
+    config = get_config()
+    if day == None and date == None:
+        date, day = get_day_date()
+    elif day != None and date != None:
+        day = day
+        date = date
+
+    try:
+        url = config["mensen"][f"{mensa}"]
+        response = requests.get(url).text
+
+        pattern = rf"{day}, {date}"
+        substrings = re.findall(rf"{pattern}(.*?)(?={pattern}|$)", response)
+        kategorien = ["Tellergericht", "Tellergericht vegetarisch", "Vegetarisch", "Klassiker", "Pizza des Tages", "Express", "Pizza Classics", "Wok", "Burger Classics", "Burger der Woche", "Hauptbeilagen", "Nebenbeilage"]
+
+        results = {}
+        gerichte_raw = {}
+        gerichte = {}
+
+        for kategorie in kategorien:
+            result = substrings[0].find(kategorie)
+            results[kategorie] = result
+
+        for i in range(len(kategorien) - 1):
+            gerichte_raw[f"{kategorien[i]}"] = substrings[0][results[f"{kategorien[i]}"]:results[f"{kategorien[i + 1]}"]]
+        gerichte_raw[f"{kategorien[-1]}"] = substrings[0][results[f"{kategorien[-1]}"]:]
+
+        for kategorie in kategorien:
+            if kategorie in gerichte_raw and len(gerichte_raw[kategorie]) != 0:
+                if "Geschlossen" in gerichte_raw[kategorie]:
+                    gerichte[kategorie] = "Geschlossen"
+                else:
+                    gerichte[kategorie] = gerichte_raw[kategorie].split(r'menue-nutr', 1)[1].split(r"</span>", 1)[1].split(r"<sup>", 1)[0].split(r"<span", 1)[0]
+            else: gerichte[kategorie] = "Fehler"
+
+        return gerichte
+    except Exception as e:
+        print("Couldn't find entries. Is the date set correct?")
+        for kategorie in kategorien:
+            gerichte[kategorie] = "Fehler"
+        return gerichte
